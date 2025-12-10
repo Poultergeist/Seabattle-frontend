5d310b3340aa7be5f5b1c32030386c995749e609
Poultergeist <vlogatskiy3@gmail.com>
2025-12-10 22:52:18 +0200

    feat(tailwind): added color scheme to index.css file, defined colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,34 @@ module.exports = {
     './node_modules/@heroui/**/*.{js,ts,jsx,tsx}',
     './node_modules/.pnpm/@heroui+*/**/*.{js,ts,jsx,tsx}', // for pnpm
   ],
+  theme: {
+    extend: {
+      colors: {
+        bg: 'var(--bg)',
+        'dark-bg': 'var(--dark-bg)',
+        'light-bg': 'var(--light-bg)',
+        border: 'var(--border)',
+
+        primary: 'var(--primary)',
+        'primary-dark': 'var(--primary-dark)',
+
+        danger: 'var(--danger)',
+        'danger-dark': 'var(--danger-dark)',
+
+        warning: 'var(--warning)',
+        'warning-dark': 'var(--warning-dark)',
+
+        success: 'var(--success)',
+        'success-dark': 'var(--success-dark)',
+
+        info: 'var(--info)',
+        'info-dark': 'var(--info-dark)',
+
+        'text-foreground': 'var(--text-foreground)',
+        'text-muted-foreground': 'var(--text-muted-foreground)',
+      },
+    },
+  },
   plugins: [
     scrollbar,
     require('tailwindcss-animate'),
