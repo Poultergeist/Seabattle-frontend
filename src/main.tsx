ac36a240ac7767fde8195f69d30f9418ce1ce16b
Poultergeist <vlogatskiy3@gmail.com>
2026-01-01 16:16:51 +0200

    feat(theme): fixed tsconfig and env vars issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import '@/app/styles/base.css';
+import './app/styles/base.css';
 
 import { App } from './App';
 import { PAGES } from './shared/config/constants/pages';
-
-console.log('Available Pages:', PAGES);
+console.group('App startup debug');
+console.log('Available pages:', PAGES);
+console.log('Environment variables:', import.meta.env);
+console.groupEnd();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
