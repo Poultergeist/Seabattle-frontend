805af853c5a75ee414a60c0d3dc9d6024e23f4d3
Poultergeist <vlogatskiy3@gmail.com>
2026-01-24 10:42:03 +0200

    feat: added mobile version of cookies banner

--- src/shared/ui/CookiesBanner/ui/CookiesBanner.tsx
+++ src/shared/ui/CookiesBanner/ui/CookiesBanner.tsx
@@ -1,6 +1,7 @@
 import { motion, AnimatePresence } from 'framer-motion';
 
 import { useSharedStore } from '@/shared/store';
+import { cn } from '@/shared/utils';
 
 import { Button } from '../../Button';
 
@@ -15,28 +16,31 @@ export const CookiesBanner = () => {
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: 12 }}
           transition={{ duration: 0.25, ease: 'easeOut' }}
-          className="bg-background fixed bottom-2 right-2 flex flex-col gap-1 rounded-lg border border-border px-8 py-4 shadow-sm"
+          className={cn(
+            'bg-background fixed inset-x-1 bottom-1 flex flex-col gap-1 rounded-lg border border-border p-2 shadow-sm md:px-8 md:py-4',
+            'md:bottom-2 md:left-auto md:right-2'
+          )}
         >
-          <h2 className="text-xl font-bold">
+          <h2 className="mb-2 text-center font-bold md:mb-0 md:text-xl">
             {'We use cookies to improve your experience on our site.'}
           </h2>
-          <p className="text-center">
+          <p className="text-center text-sm md:text-base">
             {'By using our site, you consent to cookies.'}
           </p>
-          <p className="text-center">
+          <p className="text-center text-sm md:text-base">
             {'Do we have your permission to set cookies?'}
           </p>
 
-          <div className="flex justify-center gap-2">
+          <div className="mt-2 flex flex-col justify-center gap-1 md:mt-0 md:flex-row">
             <Button
-              className="flex-1 py-6 text-lg font-bold"
+              className="flex-1 py-2 font-bold md:py-6 md:text-lg"
               onClick={() => setNot(true)}
             >
               {'Yes, you may set cookies'}
             </Button>
 
             <Button
-              className="flex-1 py-6 text-lg font-bold"
+              className="flex-1 py-2 font-bold md:py-6 md:text-lg"
               variant="outline"
               onClick={() => setNot(true)}
             >
