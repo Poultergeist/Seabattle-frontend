6b7616a82229671d79749f2252b8bc82505d6d8d
Poultergeist <vlogatskiy3@gmail.com>
2026-01-24 03:44:20 +0200

    fix: changed constant name that was loaded from environment, that crashed the app

--- src/app/AppFallback/ui/AppFallback.tsx
+++ src/app/AppFallback/ui/AppFallback.tsx
@@ -1,9 +1,19 @@
-import { Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+
+import { ThemeSwitcher } from '@/shared/ui/ThemeProvider';
 
 export const AppFallback = () => {
   return (
-    <Route path="*">
-      <h1>{'I think that you are not supposed to be here'}</h1>
-    </Route>
+    <Routes>
+      <Route
+        path="*"
+        element={
+          <>
+            <ThemeSwitcher />
+            <h1>{'I think that you are not supposed to be here'}</h1>
+          </>
+        }
+      />
+    </Routes>
   );
 };
